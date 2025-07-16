402ff634fe6143f8060f6211343a46df7c794b14
pedro558 <pedroafonso109@gmail.com>
2025-07-16 10:31:50 -0300

    :sparkles: feat: setting up the requests

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,10 +1,36 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>
 
 export function CreateRoom(){
+  const { data, isLoading} = useQuery({
+    queryKey: ['get-rooms'],
+    queryFn: async() =>{
+      const response = await fetch('http://localhost:3333/rooms');
+      const result: GetRoomsAPIResponse = await response.json()
+
+      return result
+    }
+  })
+
   return (
     <div>
       <div>Create Room</div>
+
+      {isLoading && <p>Carregando...</p>}
+
+      <div className="flex flex-col gap-1">
+        {data?.map(room => (
+          <Link key={room.id} to={`/room/${room.id}`}>
+            <Button size={"sm"} variant={"link"}>{room.name}</Button>
+          </Link>
+        ))}
+      </div>
 
       <Link to="/room">
         <Button size={"sm"} variant={"link"}>Acessar Sala</Button>
