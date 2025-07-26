c21ad91058cd5849217046d9093ce4ec55eddf93
pedro558 <pedroafonso109@gmail.com>
2025-07-25 22:22:16 -0300

    :lipstick: feat: implement 'create room form' and associated API handling

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,40 +1,17 @@
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-
-type GetRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>
+import { CreateRoomForm } from "@/components/create-room-form"
+import { RoomList } from "@/components/room-list"
 
 export function CreateRoom(){
-  const { data, isLoading} = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async() =>{
-      const response = await fetch('http://localhost:3333/rooms');
-      const result: GetRoomsAPIResponse = await response.json()
-
-      return result
-    }
-  })
-
   return (
-    <div>
-      <div>Create Room</div>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm/>
 
-      {isLoading && <p>Carregando...</p>}
+          <RoomList/>
 
-      <div className="flex flex-col gap-1">
-        {data?.map(room => (
-          <Link key={room.id} to={`/room/${room.id}`}>
-            <Button size={"sm"} variant={"link"}>{room.name}</Button>
-          </Link>
-        ))}
+        </div>
       </div>
-
-      <Link to="/room">
-        <Button size={"sm"} variant={"link"}>Acessar Sala</Button>
-      </Link>
     </div>
   )
 }
